845de61813e73e47c4172b979a7a9a4778752511
PannnTastic <pannntastic@gmail.com>
2024-04-04 17:37:09 +0800

    mengisi prosedur input

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -6,5 +6,24 @@ int n;
 int i;
 
 void input (){
-    
+    while (true)
+    {
+        cout << "Masukkan Panjang Elemen Array : ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else 
+        cout << "\nMaksimum Array Hanya 20 Elemen\n";
+    }
+
+    cout << "\n------------\n";
+    cout << "Enter Array Element\n";
+    cout << "-------------\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
